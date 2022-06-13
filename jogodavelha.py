e214aafd6949ab2c5d87e666c5a3624bbdbb09dd
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-06-13 11:13:47 -0300

    Add files via upload

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,4 +1,3 @@
-import os
 import random
 while True:
     while True:
@@ -13,24 +12,66 @@ while True:
                  [' ',' ',' ']]
 
     while True:
-        if lista[0][0] == lista[0][1] == lista[0][2] == 'x':
-            print('o x venceu')
-        if lista[1][0] == lista[1][1] == lista[1][2] =='x':
-            print('o x venceu')
-        if lista[2][0] == lista[2][1] == lista[2][2] == 'x':
-            print('o x venceu')
-        if lista[0][0] == lista[1][0] == lista[2][0] == 'x':
-            print('o x venceu')
-        if lista[0][1] == lista[1][1] == lista[2][1] == 'x':
-            print('o x venceu')
-        if lista[0][2] == lista[1][2] == lista[2][2] == 'x':
-            print('o x venceu')
-        if lista[0][0] == lista[1][1] == lista[2][2] == 'x':
-            print('o x venceu')
-        if lista[2][0] == lista[1][1] == lista[0][2] == 'x':
-            print('o x venceu')
-        else:
-            print('a letra o venceu')
+        if lista[0][0] == lista[0][1] == lista[0][2] != ' ':
+            if lista[0][0] == 'x':
+                print(f'o {lista[0][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[1][0] == lista[1][1] == lista[1][2] != ' ':
+            if lista[1][0] == 'x':
+                print(f'o {lista[1][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[2][0] == lista[2][1] == lista[2][2] != ' ':
+            if lista[2][0] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[0][0] == lista[1][0] == lista[2][0] != ' ':
+            if lista[0][0] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[0][1] == lista[1][1] == lista[2][1] != ' ':
+            if lista[0][1] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[0][2] == lista[1][2] == lista[2][2] != ' ':
+            if lista[0][2] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[0][0] == lista[1][1] == lista[2][2] != ' ':
+            if lista[0][0] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+                break
+        if lista[2][0] == lista[1][1] == lista[0][2] != ' ':
+            if lista[2][0] == 'x':
+                print(f'o {lista[2][0]} venceu')
+                break
+            else:
+                print(f'a letra o venceu')
+
+                break
+        continuar = str(input("quer continuar com o  jogo da velha[s/n]: "))[0].lower()
+        if continuar in "n":
+            break
 
         linhadopc=random.randint(0, 2)
         colunadopc =random.randint(0, 2)
@@ -46,7 +87,6 @@ while True:
         while True:
             if lista[lugarlinha][lugarcoluna] == ' ':
                 lista[lugarlinha][lugarcoluna] = opçao
-                print(1)
                 break
             else:
                 lugarlinha = int(input("qual o lugar que voce quer,\n digite a linha: ")) - 1
@@ -60,11 +100,9 @@ while True:
                 {lista[2][0]}| {lista[2][1]}| {lista[2][2]}    
                     ''')
 
-        os.system("cls")
         while True:
             if lista[linhadopc][colunadopc] == ' ':
                 lista[linhadopc][colunadopc] = opçaopc
-                print(2)
                 break
             else:
                 linhadopc = random.randint(0, 2)
@@ -78,9 +116,3 @@ while True:
         -------------
         {lista[2][0]}| {lista[2][1]}| {lista[2][2]}    
             ''')
-
-
-
-
-
-
